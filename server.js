6585fc8e4183552336f9d4e14f0ad02494a787ea
Melissa Wright <meliswright925@gmail.com>
2024-03-19 00:36:22 -0400

    updated server and config

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
 // Require express and mongoose
-const express = require('express');
-const { MongoClient } = require('mongodb');
+const express = require("express");
 const db = require("./config/connection");
 const routes = require("./routes");
 
-//Environment Variables 
+//Environment Variables
 const app = express();
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
- //Middleware to parse incoming data
+//Middleware to parse incoming data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Use routes defined in routes.js
-app.use(routes); 
+app.use(routes);
 
 // Connect to the MongoDB database and start the server
-db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-    });
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
   });
+});
