b84799364202f236d11446cf568b4cf5e9387629
Melissa Wright <meliswright925@gmail.com>
2024-03-19 01:04:13 -0400

    user routes

---
+++ routes/userRoutes.js
@@ -0,0 +1,17 @@
+// routes/userRoutes.js
+
+const router = require("express").Router();
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/userController");
+
+// Routes
+router.route("/").get(getAllUsers).post(createUser);
+
+router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+
+module.exports = router;
