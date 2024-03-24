b8b471b10d2e059594359768721ad9e2a5f33867
Melissa Wright <meliswright925@gmail.com>
2024-03-24 16:31:20 -0400

    added controller for adding and deleting friend

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,13 @@
 const User = require("../models/User");
 
 module.exports = {
-  getAllUsers: async (req, res) => {
+  getAllUsers: async (_req, res) => {
+    // Find all users
     try {
       const users = await User.find();
       res.json(users);
     } catch (err) {
-      res.status(500).json(err);
+      res.status(500).json(err); // Handle errors
     }
   },
   getUserById: async (req, res) => {
@@ -14,7 +15,7 @@ module.exports = {
       const user = await User.findById(req.params.id);
       res.json(user);
     } catch (err) {
-      res.status(500).json(err);
+      res.status(500).json(err); // Handle errors
     }
   },
   createUser: async (req, res) => {
@@ -35,12 +36,49 @@ module.exports = {
       res.status(400).json(err); // Handle validation errors
     }
   },
-  deleteUser: async (req, res) => {
+  deleteUserByID: async (req, res) => {
+    //changed to function
     try {
-      await User.findByIdAndDelete(req.params.id); // Find and delete the user
+      const user = await User.findByIdAndDelete(req.params.id); // Changed to findByIdAndDelete
+      if (!user) {
+        return res.status(404).json({ message: "user not found" });
+      }
       res.json({ message: "User deleted successfully" }); // Send a success message
     } catch (err) {
       res.status(400).json(err); // Handle errors
+    }
+  },
+
+  addFriend: async (req, res) => {
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $addToSet: { friends: req.params.friendId } },
+        { new: true }
+      );
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  deleteFriend: async (req, res) => {
+    // changed this to async and await and to use pull for delete just like update
+    try {
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $pull: { friends: req.params.friendId } },
+        { new: true }
+      );
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
     }
   },
 };
