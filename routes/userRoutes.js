19a8d26b4d68ca472eb74d35418132d0237d5592
Melissa Wright <meliswright925@gmail.com>
2024-03-25 23:42:00 -0400

    added friends route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,12 +6,15 @@ const {
   getUserById,
   createUser,
   updateUser,
-  deleteUser,
+  deleteUserByID,
+  addFriend,
 } = require("../controllers/userController");
 
 // Routes
 router.route("/").get(getAllUsers).post(createUser);
 
-router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+router.route("/:id").get(getUserById).put(updateUser).delete(deleteUserByID);
+
+router.route("/:userId/friends/:friendId").post(addFriend);
 
 module.exports = router;
